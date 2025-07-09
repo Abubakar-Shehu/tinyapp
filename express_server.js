65d79c508079c0ff0de4ab28691fa910e3e6c159
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-07-09 17:47:17 -0600

    added a form for regristration of email and password but not post route on the server yet

--- express_server.js
+++ express_server.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const cookieParser = require('cookie-parser');
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-
-app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -27,6 +28,13 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+// app.get("/urls", (req, res) => {
+//   const templateVars = {
+//     username: req.cookies["username"],
+//   };
+//   res.render("urls_index", templateVars);
+// });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -66,6 +74,10 @@ app.post("/login", (req, res) => {
   res.cookie('username', req.body.username);
   res.redirect("/urls")
 });
+
+app.get("/register", (req, res) => {
+  res.render("register")
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
