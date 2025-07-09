f25625ab3a5b9acab35038cf66c85e1070b09a4f
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-07-09 17:59:38 -0600

    would come back to you

--- express_server.js
+++ express_server.js
@@ -25,16 +25,12 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+   };
   res.render("urls_index", templateVars);
 });
-
-// app.get("/urls", (req, res) => {
-//   const templateVars = {
-//     username: req.cookies["username"],
-//   };
-//   res.render("urls_index", templateVars);
-// });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
