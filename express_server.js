ca2c08659ce8db2964fb9b9cf88d2d3c40a23460
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-07-08 18:52:08 -0600

    IMplemented an edit button but might need to come back to this

--- express_server.js
+++ express_server.js
@@ -56,6 +56,10 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect(`/urls`); 
 });
 
+app.post("/urls/:id", (req, res) => {
+  res.redirect(`/urls/${req.params.id}`);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
